5c585d44079ef33f06a8328802f2dfecd150633f
RealNapsta <yeungbuc@gmail.com>
2020-03-20 16:48:57 +0000

    Added Employers upload job pages

---
+++ The Job Box/Controllers/EmployerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace The_Job_Box.Controllers
+{
+    public class EmployerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult RealBasic()
+        {
+            return View();
+        }
+
+        public IActionResult RealStandard()
+        {
+            return View();
+        }
+
+        public IActionResult RealUltimate()
+        {
+            return View();
+        }
+    }
+}
